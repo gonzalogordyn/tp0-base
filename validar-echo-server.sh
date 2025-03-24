56e6d061c05fc1fe715fc09bc581a752d3fa5997
Gonzalo Gordyn Biello <ggordyn@fi.uba.ar>
2025-03-24 13:09:36 -0300

    retrieveo el server ip

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -4,7 +4,9 @@ SERVER_CONTAINER="server"
 PORT=12345
 MSG="hola mundo 1234"
 
-RESPONSE=$(docker run --rm --network container:$SERVER_CONTAINER alpine sh -c "echo "$MSG" | nc -w 5 localhost $PORT")
+SERVER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $SERVER_CONTAINER)
+
+RESPONSE=$(docker run --rm --network container:$SERVER_CONTAINER alpine sh -c "echo "$MSG" | nc -w 5 $SERVER_IP $PORT")
 
 if [ "$RESPONSE" = "$MSG" ]; then
     echo "action: test_echo_server | result: success"
