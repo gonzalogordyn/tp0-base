95d74054b9baf0a8d43244bdc525968eda79758a
Gonzalo Gordyn Biello <ggordyn@fi.uba.ar>
2025-03-26 02:25:11 -0300

    modificacion de cliente

--- client/common/client.go
+++ client/common/client.go
@@ -1,9 +1,11 @@
 package common
 
 import (
-	"bufio"
-	"fmt"
+	"encoding/binary"
+	"io"
 	"net"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/op/go-logging"
@@ -58,40 +60,97 @@ func (c *Client) GracefulShutdown() {
 	}
 }
 
+func (c *Client) WriteAllBytes(data []byte) error {
+	totalSent := 0
+	for totalSent < len(data) {
+		sent, err := c.conn.Write(data[totalSent:])
+		if err != nil {
+			return err
+		}
+		if sent == 0 {
+			return io.ErrUnexpectedEOF
+		}
+		totalSent += sent
+	}
+	return nil
+}
+
+func (c *Client) ReadAllBytes(n int) ([]byte, error) {
+	data := make([]byte, n)
+	totalRead := 0
+	for totalRead < n {
+		read, err := c.conn.Read(data[totalRead:])
+		if err != nil {
+			return nil, err
+		}
+		if read == 0 {
+			return nil, io.ErrUnexpectedEOF
+		}
+		totalRead += read
+	}
+	return data, nil
+}
+
 // StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
 	// There is an autoincremental msgID to identify every message sent
 	// Messages if the message amount threshold has not been surpassed
-	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
-		// Create the connection the server in every loop iteration. Send an
-		c.createClientSocket()
+	// Create the connection the server in every loop iteration. Send an
+	c.createClientSocket()
+	nombre := os.Getenv("NOMBRE")
+	apellido := os.Getenv("APELLIDO")
+	nacimiento := os.Getenv("NACIMIENTO")
+	documentoStr := os.Getenv("DOCUMENTO")
+	numeroStr := os.Getenv("NUMERO")
 
-		// TODO: Modify the send to avoid short-write
-		fmt.Fprintf(
-			c.conn,
-			"[CLIENT %v] Message N°%v\n",
-			c.config.ID,
-			msgID,
-		)
-		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-		c.conn.Close()
-
-		if err != nil {
-			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-				c.config.ID,
-				err,
-			)
-			return
-		}
-
-		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-			c.config.ID,
-			msg,
-		)
-
-		// Wait a time between sending one message and the next one
-		time.Sleep(c.config.LoopPeriod)
-
+	documento, err := strconv.ParseUint(documentoStr, 10, 32)
+	if err != nil {
+		log.Errorf("error parseando documento")
 	}
-	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+	numero, err := strconv.ParseUint(numeroStr, 10, 32)
+	if err != nil {
+		log.Errorf("error parseando numero")
+	}
+
+	// Creo el paquete
+	packet, err := NewPacket(nombre, apellido, nacimiento, uint32(documento), uint32(numero))
+	if err != nil {
+		log.Errorf("error creando paquete")
+	}
+
+	// Lo serializo a bytes
+	packetBytes, err := packet.Serialize()
+	if err != nil {
+		log.Errorf("error serializando paquete")
+	}
+
+	// Escribo con funcion auxiliar para evitar short write
+	err = c.WriteAllBytes(packetBytes)
+	if err != nil {
+		log.Errorf("error enviando paquete")
+	}
+
+	// Lectura de la response con funcion auxiliar para evitar short read
+	msg, err := c.ReadAllBytes(7)
+	if err != nil {
+		log.Errorf("error recibiendo respuesta")
+	}
+
+	if len(msg) != 7 || string(msg[:3]) != "ACK" {
+		log.Errorf("error recibiendo respuesta")
+		return
+	}
+
+	receivedNumero := binary.BigEndian.Uint32(msg[3:])
+	if receivedNumero != packet.Numero {
+		log.Errorf("numero incorrecto, esperado: %d, recibido: %d", packet.Numero, receivedNumero)
+		return
+	}
+
+	c.conn.Close()
+
+	log.Infof("action: apuesta_enviada | result: success | dni: %v | numero: %v",
+		packet.Documento,
+		packet.Numero,
+	)
 }
