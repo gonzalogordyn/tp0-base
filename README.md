4d5deb891887d4934b011a016e6b0678411195c3
Gonzalo Gordyn Biello <ggordyn@fi.uba.ar>
2025-03-27 18:38:05 -0300

    cambios en server, no funciona todavia

--- README.md
+++ README.md
@@ -1,28 +1,12 @@
 # TP0: Docker + Comunicaciones + Concurrencia
 
-## Parte 2: Repaso de Comunicaciones
+## Parte 3: Repaso de Concurrencia
+En este ejercicio es importante considerar los mecanismos de sincronización a utilizar para el correcto funcionamiento de la persistencia.
 
-Las secciones de repaso del trabajo práctico plantean un caso de uso denominado **Lotería Nacional**. Para la resolución de las mismas deberá utilizarse como base el código fuente provisto en la primera parte, con las modificaciones agregadas en el ejercicio 4.
+### Ejercicio N°8:
 
-### Ejercicio N°7:
-
-Modificar los clientes para que notifiquen al servidor al finalizar con el envío de todas las apuestas y así proceder con el sorteo.
-Inmediatamente después de la notificacion, los clientes consultarán la lista de ganadores del sorteo correspondientes a su agencia.
-Una vez el cliente obtenga los resultados, deberá imprimir por log: `action: consulta_ganadores | result: success | cant_ganadores: ${CANT}`.
-
-El servidor deberá esperar la notificación de las 5 agencias para considerar que se realizó el sorteo e imprimir por log: `action: sorteo | result: success`.
-Luego de este evento, podrá verificar cada apuesta con las funciones `load_bets(...)` y `has_won(...)` y retornar los DNI de los ganadores de la agencia en cuestión. Antes del sorteo no se podrán responder consultas por la lista de ganadores con información parcial.
-
-Las funciones `load_bets(...)` y `has_won(...)` son provistas por la cátedra y no podrán ser modificadas por el alumno.
-
-No es correcto realizar un broadcast de todos los ganadores hacia todas las agencias, se espera que se informen los DNIs ganadores que correspondan a cada una de ellas.
+Modificar el servidor para que permita aceptar conexiones y procesar mensajes en paralelo. En caso de que el alumno implemente el servidor en Python utilizando _multithreading_,  deberán tenerse en cuenta las [limitaciones propias del lenguaje](https://wiki.python.org/moin/GlobalInterpreterLock).
 
 ## Resolución
-En este ejercicio me valí de los ejercicios anteriores donde el servidor lee los primeros dos bytes para obtener el tamaño del payload de los paquetes. Utilicé este mismo campo para enviar la notificación desde el cliente de que se han enviado todas las apuestas, en forma de un int de 2 bytes con la constante `9000`. Elegí este número ya que el length que usualmente ocupa estos primeros dos bytes no superará los 8kb, y al ser 2 bytes tengo un gran rango de números superando el 8192 para utilizar a mi placer.
 
-Utilicé el mismo campo para el código que el servidor le envía al cliente informando que el paquete contiene los ganadores del sorteo, esta vez con la constante `9001`. Los dos siguientes bytes serán para la longitud en bytes del payload, que es la serie de documentos ganadores (se irá leyendo de a 4 bytes hasta llegar al final).
-
-También tuve que incluir un mensaje al realizar la conexión, donde el cliente enviará en un byte su id, para que el servidor lo pueda almacenar en un diccionario que lo mappea con el socket, y pueda comunicarse y mandarle el mensaje específico de sus ganadores al finalizar el sorteo.
-
-La comunicación se realiza de manera secuencial, es decir que cada cliente se maneja de a uno por vez. Se mantienen sus conexiones abiertas hasta finalizar el sorteo, luego de lo cual se cerrarán todos los sockets correspondientes (no sin antes comunicar los ganadores).  
-Los clientes, por su parte, quedarán bloqueados esperando un mensaje del servidor con los ganadores luego de enviar la notificación de que ya han mandado todas las apuestas. Luego de procesarlo, cerrarán la conexión y finalizarán su ejecución.
+Decidí optar por multiprocesamiento para esquivar el GIL y poder aprovechar un paralelismo real.
--- server/common/server.py
+++ server/common/server.py
@@ -2,6 +2,7 @@ import os
 import socket
 import logging
 import signal
+from multiprocessing import Process, Manager, Lock, Queue, Value
 from common.packet import Packet
 from common.batch import Batch
 from common.utils import *
@@ -14,85 +15,95 @@ class Server:
     def __init__(self, port, listen_backlog):
         # Initialize server socket
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._server_socket.settimeout(1)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
         self._clients = {}
-        self._notified = 0
+        self._queue = Queue()
+        self._notified = Value('i', 0)
 
         try:
             self._num_agencias = int(os.getenv("AGENCIAS", 5))
-            # logging.debug(f"Cantidad de agencias: {self._num_agencias}")
         except ValueError:
             logging.error("Valor inválido.")
             raise
-        self._ganadores = {str(i): [] for i in range(1, self._num_agencias + 1)}
+        with Manager() as manager:
+            self._ganadores = manager.dict({str(i): [] for i in range(1, self._num_agencias + 1)})
+
+        self._lock = Lock()
 
     def run(self):
         """
-        Dummy Server loop
-
-        Server that accept a new connections and establishes a
-        communication with a client. After client with communucation
-        finishes, servers starts to accept new connections again
+        Por cada conexión se lanza un nuevo proceso.
+        El main process se va a encargar de comunicarle al resto los ganadores.
         """
-
         while True:
-            id = self.__accept_new_connection()
-            self.__handle_client_connection(id)
+            logging.debug("queue size: %s", self._queue.qsize())
+            if not self._queue.empty():
+                message = self._queue.get()
+                if message == "EXIT":
+                    logging.debug("Sorteo terminado")
+                    break
 
-    def __recv_all_bytes(self, id):
-        packet_bytes = b''
-        packet_length_bytes = self._clients[id].recv(2)
-        packet_bytes += packet_length_bytes
-        packet_length = int.from_bytes(packet_length_bytes, byteorder='big', signed=False)
-
-        # logging.debug(f"action: receive_message | length: {packet_length}")
-
-        if packet_length == self.NOTIFY_FINISHED:
-            # logging.debug(f"action: Recibió un finished")
-            return packet_bytes, "FINISHED"
+            client_id, client_socket = self.__accept_new_connection()
+            
+            if client_id is not None and client_socket is not None:
+                process = Process(target=self.__handle_client_connection, args=(client_id, client_socket))
+                process.start()
         
-        while len(packet_bytes) - 2 < packet_length:
-            received = self._clients[id].recv(packet_length - len(packet_bytes) + 2)
-            if not received:
-                return packet_bytes
-            packet_bytes += received
-        return packet_bytes, "OK"
-
-    def __write_all_bytes(self, data, id):
-        # logging.debug(f"sending message to client {str(id)}")
-        # logging.debug(f"clients: {self._clients}")
-        sent_bytes = 0
-        while sent_bytes < len(data):
-            sent = self._clients[id].send(data[sent_bytes:])
-            if sent == 0:
-                logging.error("Error al enviar datos")
-                return
-            sent_bytes += sent
-        return
-    
-    def __handle_notificaciones(self):
-        self._notified += 1
-        if self._notified == self._num_agencias:
-            logging.info("action: sorteo | result: success")
-            self.__handle_sorteo()
-
-    def __handle_sorteo(self):
-        bets = load_bets()
-        for bet in bets:
-            if has_won(bet):
-                agency_key = str(bet.agency)
-                self._ganadores[agency_key].append(bet.document)
-
         for agency, winners in self._ganadores.items():
             winners_packet = WinnersPacket(winners)
             winners_bytes = winners_packet.serialize()
             self.__write_all_bytes(winners_bytes, agency)
         
         self.graceful_shutdown()
+        
 
+    def __recv_all_bytes(self, client_socket):
+        packet_bytes = b''
+        packet_length_bytes = client_socket.recv(2)
+        packet_bytes += packet_length_bytes
+        packet_length = int.from_bytes(packet_length_bytes, byteorder='big', signed=False)
 
-    def __handle_client_connection(self, client_id):
+        logging.debug(f"Longitud del paquete recibido: {packet_length}")
+        if packet_length == self.NOTIFY_FINISHED:
+            return packet_bytes, "FINISHED"
+        
+        while len(packet_bytes) - 2 < packet_length:
+            received = client_socket.recv(packet_length - len(packet_bytes) + 2)
+            if not received:
+                return packet_bytes
+            packet_bytes += received
+        return packet_bytes, "OK"
+
+    def __write_all_bytes(self, data, client_socket):
+        sent_bytes = 0
+        while sent_bytes < len(data):
+            sent = client_socket.send(data[sent_bytes:])
+            if sent == 0:
+                logging.error("Error al enviar datos")
+                return
+            sent_bytes += sent
+
+    def __handle_notificaciones(self):
+        with self._lock:
+            self._notified.value += 1
+            logging.debug("Notificacion recibida | total: %s | numero agencias: %s", self._notified.value, self._num_agencias)
+            if self._notified.value == self._num_agencias:
+                logging.info("action: sorteo | result: success")
+                self.__handle_sorteo()
+
+    def __handle_sorteo(self):
+        bets = load_bets()
+        for bet in bets:
+            if has_won(bet):
+                agency_key = str(bet.agency)
+                with self._lock:
+                    self._ganadores[agency_key].append(bet.document)
+        logging.debug(f"Poniendo EXIT en queue")
+        self._queue.put("EXIT")
+
+    def __handle_client_connection(self, client_id, client_socket):
         """
         Read message from a specific client socket and closes the socket
 
@@ -101,59 +112,54 @@ class Server:
         """
         try:
             while True:
-                received_bytes, status = self.__recv_all_bytes(client_id)
-                # logging.info(f'Received bytes: {received_bytes}')
+                received_bytes, status = self.__recv_all_bytes(client_socket)
 
                 if status == "FINISHED":
-                    # logging.info(f"action: notificacion | result: success | client: {client_id}")
                     self.__handle_notificaciones()
                     break
                 else:
                     batch, failed_packets = Batch.deserialize(received_bytes)
-                    # logging.info(f'Batch: {batch.packets}')
-                    
+                    bets = []
                     for packet in batch.packets:
                         bet = Bet(client_id, packet.nombre, packet.apellido, packet.documento, packet.nacimiento, packet.numero)
-                        store_bets([bet])
-                        # logging.info(f'action: apuesta_almacenada | result: success | client: {client_id} |  nombre: {packet.nombre} {packet.apellido} | dni: {packet.documento} | numero: {packet.numero}')
+                        bets = []
+                        bets.append(bet)
                     
+                    with self._lock:
+                        store_bets([bet])
 
                     if failed_packets > 0:
                         logging.error(f'action: apuesta_recibida | result: fail | cantidad: {failed_packets}')
                         response = "ERR".encode('utf-8')
-                        self.__write_all_bytes(response, client_id)
+                        self.__write_all_bytes(response, client_socket)
                     else:
                         logging.info(f'action: apuesta_recibida | result: success | cantidad: {len(batch.packets)}')
                         response = "ACK".encode('utf-8')
-                        self.__write_all_bytes(response, client_id)
-                
+                        self.__write_all_bytes(response, client_socket)
         except OSError as e:
-            logging.error("action: receive_message | result: fail | error: {e}")
-        # finally:
-        #     self._clients[client_id].close()
+            logging.error(f"action: receive_message | result: fail | error: {e}")
 
     def __accept_new_connection(self):
         """
-        Accept new connections
-
-        Function blocks until a connection to a client is made.
-        Then connection created is printed and returned
+        Acepta nuevas conexiones, y tiene un timeout.
         """
+        try:
+            logging.info('action: accept_connections | result: in_progress')
+            client_socket, addr = self._server_socket.accept()
+            logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
+            
+            client_id_bytes = client_socket.recv(1)
+            client_id = int.from_bytes(client_id_bytes, byteorder='big', signed=False)
+            client_id_str = str(client_id)
 
-        # Connection arrived
-        logging.info('action: accept_connections | result: in_progress')
-        c, addr = self._server_socket.accept()
-        logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
+            logging.debug(f"ID de cliente recibido: {client_id_str}")
+            self._clients[client_id_str] = client_socket
+            
+            return client_id_str, client_socket
         
-        client_id_bytes = c.recv(1)
-        client_id = int.from_bytes(client_id_bytes, byteorder='big', signed=False)
-        client_id_str = str(client_id)
-        # logging.info(f'id: {client_id_str}')
-        
-        self._clients[client_id_str] = c
+        except socket.timeout:
+            return None, None
 
-        return client_id_str
-    
     def graceful_shutdown(self):
         logging.debug("Cerrando servidor...")
         
@@ -165,7 +171,6 @@ class Server:
                 logging.error(f"Error cerrando socket del cliente con ID: {client_id} | Error: {e}")
         
         self._clients.clear()
-
         logging.debug("Cerrando socket del servidor")
         self._server_socket.close()
         sys.exit(0)
